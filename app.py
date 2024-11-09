b4fcc56ab8025dd131a3b5378ab9a8df81707c52
meryzennn <85497760+meryzennn@users.noreply.github.com>
2024-11-09 22:38:23 +0700

    change app

--- app.py
+++ app.py
@@ -1,36 +1,25 @@
-from flask import Flask, render_template, request, jsonify
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, classification_report
 
-app = Flask(__name__)
-
-# Load data and model components
 data = pd.read_csv("data_spam.csv", encoding="latin-1")
-data = data[["class", "message"]]
+data.head()
+
+data = data[["class","message"]]
+
 x = np.array(data["message"])
 y = np.array(data["class"])
-
 cv = CountVectorizer()
+
 X = cv.fit_transform(x)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=33)
 
 deteksi = MultinomialNB()
-deteksi.fit(X, y)
+deteksi.fit(X_train, y_train)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    data = request.get_json()
-    input_message = data["message"]
-    transformed_input = cv.transform([input_message]).toarray()
-    prediction = deteksi.predict(transformed_input)
-    
-    result = "spam" if prediction == "spam" else "ham"
-    return jsonify({"result": result})
-
-if __name__ == "__main__":
-    app.run(debug=True)
+inputan = input("Masukkan teks: ")
+data = cv.transform([inputan]).toarray()
+print(deteksi.predict(data))
